870a1b0299433b94966da4bda36983a64153e845
sarandevarjun <saransri13@gmail.com>
2025-03-02 14:33:51 +0530

    fix the enroll button changes

--- src/components/EnrollButton.js
+++ src/components/EnrollButton.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const EnrollButton = () => {
   return (
-    <div className="rbt-btn-wrapper">
-      <a
+        <div className="rbt-btn-wrapper">
+          <a
         className="rbt-btn rbt-marquee-btn marquee-auto btn-border-gradient rounded-full text-sm transform-none"
-        href="#"
-      >
+        href="/enroll"
+        >
         <span data-text="Enroll Now">Enroll Now</span>
-      </a>
+        </a>
     </div>
   );
 };
